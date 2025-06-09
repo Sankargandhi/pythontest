eb47fe40185bd9ba528a05d311e72a8b70ba5aba
admin <sankar.gandhi@gmail.com>
2025-06-09 20:01:04 +0530

    temp2

--- testapp/deploy.sh
+++ testapp/deploy.sh
@@ -10,10 +10,11 @@ echo "🚀 Starting deployment to EC2..."
 echo "Creating PEM file"
 printf "%s\n" "$EC2_SSH_KEY" > ec2_key.pem
 
-echo "Changing the mode"
-chmod 600 ec2_key.pem
+
+#chmod 600 ec2_key.pem
 
 # Connect and deploy
+#echo "Connect and deploy"
 ssh -i ec2_key.pem -o StrictHostKeyChecking=no $EC2_SSH_USER@$EC2_HOST << 'EOF'
   set -e
   echo "🔄 Pulling latest changes..."
