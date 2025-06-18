0e3a372913afee9b6756985030754f848440ba41
admin <sankar.gandhi@gmail.com>
2025-06-18 17:09:11 +0530

    demo

--- testapp/test.py
+++ testapp/test.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def home():
-    return "CI-CD Implementation : 00:42 14-06-2025"
+    return "CI-CD Implementation : 17:08 18-06-2025"
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
